5b56c33431625998eaab234f5dfaf3cc3c97bba5
amtp89 <am.tp.1989@gmail.com>
2022-08-08 01:17:12 +0100

    Installed Umbraco

--- UmbracoTest/Program.cs
+++ UmbracoTest/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace UmbracoTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
